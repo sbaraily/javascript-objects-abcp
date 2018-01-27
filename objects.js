819326977cab55d695cce608fe7ae64730b10536
sbaraily <samir.baraily@gmail.com>
2018-01-27 02:28:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {norah : 'Come away with me' , jason: 'I am yours'}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  Object.assign({artistName: songTitle})
+  playlist[artistName] = songTitle
   return Object.assign({},playlist, songTitle)
 }
