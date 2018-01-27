24480816c781680e2eaac6c6db613684e9693d35
sbaraily <samir.baraily@gmail.com>
2018-01-27 02:26:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {norah : 'Come away with me' , jason: 'I am yours'}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  Object.assign({shareen: 'Shape of you'})
+  Object.assign({artistName: songTitle})
   return Object.assign({},playlist, songTitle)
 }
